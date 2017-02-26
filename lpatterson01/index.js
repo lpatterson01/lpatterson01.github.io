4f343956e5d4ababd9e756485ae773ec2d21b2e1
lpatterson01 <lpatterson01@mica.edu>
2017-02-25 19:19:24 -0500

    revision
    revision of population site

--- lpatterson01/index.js
+++ lpatterson01/index.js
@@ -15,16 +15,18 @@ var json = (function () {
           return $("#urban_population").text('Sorry!  There was an error requesting your data.')
         }
     });
+    debugger
     return json
 })();
-
-for(var i = 0; i < json.length; i++) {
-  // $('#dinosaurs').append('<li>' + json[i].nm + '</li>');
-  $('#urban_population').append("<li class='urban_population' id='" + json[i].rnk + "'>" + json[i].rnk + "</li>");
-  $('#urban_population').append("<li class='urban_population' id='" + json[i].cn + "'>" + json[i].cn + "</li>");
-  $('#urban_population').append("<li class='urban_population' id='" + json[i].cd + "'>" + json[i].cd + "</li>");
-  $('#urban_population').append("<li class='urban_population' id='" + json[i].yr + "'>" + json[i].yr + "</li>");
-  $('#urban_population').append("<li class='urban_population' id='" + json[i].pop + "'>" + json[i].pop + "</li>");
-  $('#urban_population').append("<li class='urban_population' id='" + json[i].pct + "'>" + json[i].pct + "</li>");
-  $('#urban_population').append("<li class='urban_population' id='" + json[i].fr + "'>" + json[i].fr + "</li>");
-}
+var template = urban_population.innerHTML;
+target.innerHTML = _.template(template,{json:json});
+// for(var i = 0; i < json.length; i++) {
+//   // $('#dinosaurs').append('<li>' + json[i].nm + '</li>');
+//   $('#urban_population').append("<li class='urban_population' id='" + json[i].rnk + "'>" + json[i].rnk + "</li>");
+//   $('#urban_population').append("<li class='urban_population' id='" + json[i].cn + "'>" + json[i].cn + "</li>");
+//   $('#urban_population').append("<li class='urban_population' id='" + json[i].cd + "'>" + json[i].cd + "</li>");
+//   $('#urban_population').append("<li class='urban_population' id='" + json[i].yr + "'>" + json[i].yr + "</li>");
+//   $('#urban_population').append("<li class='urban_population' id='" + json[i].pop + "'>" + json[i].pop + "</li>");
+//   $('#urban_population').append("<li class='urban_population' id='" + json[i].pct + "'>" + json[i].pct + "</li>");
+//   $('#urban_population').append("<li class='urban_population' id='" + json[i].fr + "'>" + json[i].fr + "</li>");
+// }
